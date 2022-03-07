0e9f9639f1d1eaa800b3354854568bd18d19b649
vildan-b <vildanb@gmx.de>
2022-03-07 14:08:12 +0100

    memory

---
+++ src/memory.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="text-center">
+
+<div  class="test"><slot name="heading"></slot></div>
+
+<div  class="test"><slot name="content"></slot></div>
+
+</div></template>
+
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+div.test {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px #666;
+    padding: 20px 20px;
+    margin-top: 20px;
+
+}
+</style>
